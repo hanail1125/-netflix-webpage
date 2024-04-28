9d6b09631eb6468154df19627dafa99bac5255a0
hanail1125 <hanail011250@gmail.com>
2024-04-29 03:28:11 +0900

    API 주소 간단 수정

--- src/hooks/useMoviesQuery.js
+++ src/hooks/useMoviesQuery.js
@@ -4,7 +4,7 @@ import api from '../utils/api';
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const fetchMovies = async (movieType) => {
-  return await api.get(`/movie/${movieType}?language=ko-KR&api_key=${API_KEY})`);
+  return await api.get(`/movie/${movieType}?language=ko-KR&api_key=${API_KEY}`);
 };
 
 export const useMoviesQuery = (movieType) => {
