a4e8dcefbffcfab98c56b1e5276a83924b77fea4
hanail1125 <hanail011250@gmail.com>
2024-04-29 03:18:07 +0900

    NetFlix WebPage 코딩알려주는 누나에게 공유 - 깃 커밋 : api 주소에 API_KEY 추가

--- src/hooks/useMoviesGenre.js
+++ src/hooks/useMoviesGenre.js
@@ -1,8 +1,10 @@
 import { useQuery } from '@tanstack/react-query';
 import api from '../utils/api';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 const fetchMovieGenre = async () => {
-  return await api.get(`/genre/movie/list?language=ko-KR`);
+  return await api.get(`/genre/movie/list?language=ko-KR&api_key=${API_KEY}`);
 };
 
 export const useMoviesGenreQuery = () => {
